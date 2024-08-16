ff5727494b256b804fe5b9422b042f87a4ef5e28
趙蔚瀅 <sara.chao@imsoft.com.tw>
2024-08-16 23:32:41 +0800

    refactor: delete comment

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.vue';
 import router from './router'
 
 import axios from 'axios';
-// const instance = axios.create({
-//     baseURL: 'http://localhost:9000/voteSystem', // 基礎 URL
-//     headers: {'Content-Type': 'application/json'}
-// });
+
 axios.defaults.baseURL = 'http://localhost:9000/voteSystem';
 const app = createApp(App).use(router);
 
